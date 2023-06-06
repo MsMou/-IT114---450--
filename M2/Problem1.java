d48e10099910d588ede665a7ad5d04a62065b15b
MsMou <125760059+MsMou@users.noreply.github.com>
2023-06-05 23:31:38 -0400

    adding template baselines

---
+++ M2/Problem1.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Problem1 {
+    public static void main(String[] args) {
+        //Don't edit anything here
+        int[] a1 = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] a2 = new int[]{0, 1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
+        int[] a3 = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        int[] a4 = new int[]{0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10};
+        
+        processArray(a1);
+        processArray(a2);
+        processArray(a3);
+        processArray(a4);
+    }
+    static void processArray(int[] arr){
+        System.out.println("Processing Array:" + Arrays.toString(arr));
+        System.out.println("Odds output:");
+        //creating a for loop to iterate through each values
+        for(int x = 0; x <arr.length; x++)
+        //this give any value divided in two leaving the remainder
+            if(arr[x] % 2 == 1){
+                System.out.println(arr[x]);
+            }
+        //TODO add/edit code here of one to gie all odd number
+    
+        //end add/edit section
+        System.out.println();
+        System.out.println("End process");
+    }
+    
+}
